d50628b4b6b2a0f5df2ac65a9c77d18261138626
wamuni <boshi.pan@outlook.com>
2024-04-13 22:39:21 +0100

    implement RetrieveFeeds function in feed.go file

--- search/feed.go
+++ search/feed.go
@@ -6,3 +6,22 @@ import (
 )
 
 const dataFile = "data/data.json"
+
+type Feed struct {
+	Name string `json:"site"`
+	URL  string `json:"link"`
+	Type string `json:"type"`
+}
+
+func RetrieveFeeds() ([]*Feed, error) {
+	file, err := os.Open(dataFile)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+	var feeds []*Feed
+	err = json.NewDecoder(file).Decode(&feeds)
+
+	return feeds, err
+}
